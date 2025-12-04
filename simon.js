1c30ab05a4d701159bde3ac5a4941fc5fd969f55
David Páez <142960018+Dakewer@users.noreply.github.com>
2025-12-04 17:13:34 -0600

    game finished

--- simon.js
+++ simon.js
@@ -2,7 +2,7 @@ const simonButtons = document.getElementsByClassName("tile");
 const startButton = document.getElementById("start");
 
 class Simon {
-    constructor(simonButton, startButton) {
+    constructor() {
         this.round = 0;
         this.totalRounds = 1000000;
         this.userPosition = 0;
@@ -13,89 +13,107 @@ class Simon {
         this.display = startButton;
     }
 
-    //Funciones
-    init(){
+    init() {
         this.display.onclick = () => this.startGame();
+
+        // Activar los botones
+        this.buttons.forEach((btn, index) => {
+            btn.onclick = () => this.buttonClick(index);
+        });
     }
 
-    startGame(){
+    startGame() {
         this.display.disabled = true;
-        this.updateRound(0);
+        this.round = 0;
         this.userPosition = 0;
+        this.speed = 1000;
         this.sequence = this.createSequence();
         this.showSequence();
     }
 
-    updateRound(value){
-        this.round = value;
+    createSequence() {
+        return Array.from({ length: this.totalRounds }, () => this.getRandomColor());
     }
 
-    createSequence(){
-        return Array.from({length: this.totalRounds}, () => this.getRandomColor());
-    }
-
-    getRandomColor(){
+    getRandomColor() {
         return Math.floor(Math.random() * 4);
     }
 
-    buttonClick(value){
-        !this.blockedButtons && this.validateChosenColor(value);
+    buttonClick(value) {
+        if (!this.blockedButtons) {
+            this.validateChosenColor(value);
+        }
     }
 
-    validateChosenColor(value){
-        if(this.sequence[this.userPosition] === value){
-            //Ver si se agrega sonido o no
-            if(this.round === this.userPosition){
-                this.updateRound(this.round + 1);
+    validateChosenColor(value) {
+        // Si el jugador acierta el color correcto
+        if (this.sequence[this.userPosition] === value) {
+
+            this.userPosition++;
+
+            // Fin de la ronda
+            if (this.userPosition > this.round) {
+                this.round++;
                 this.speed /= 1.02;
                 this.isGameOver();
             }
-        } else{
+
+        } else {
             this.gameLost();
         }
     }
 
-    isGameOver(){
-        if(this.round === this.totalRounds){
+    isGameOver() {
+        if (this.round === this.totalRounds) {
             this.gameWon();
-        } else{
+        } else {
             this.userPosition = 0;
             this.showSequence();
         }
     }
 
-    showSequence(){
+    showSequence() {
         this.blockedButtons = true;
+        // Pone la secuencia hasta el inicio para que se repita
         let sequenceIndex = 0;
+
         let timer = setInterval(() => {
             const button = this.buttons[this.sequence[sequenceIndex]];
-            // En caso de usar sonido aquí se debe reproducir el sonido del botón en el que esté actualmente
-            this.toggleButtonStyle(button)
-            setTimeout(() => this.toggleButtonStyle(button), this.speed / 2)
-            sequenceIndex ++;
-            if(sequenceIndex > this.round){
-                this.blockedButtons = false;
+
+            this.toggleButtonStyle(button);
+            setTimeout(() => this.toggleButtonStyle(button), this.speed / 2);
+
+            sequenceIndex++;
+
+            // Cuando ya se mostraron todos los colores de la ronda
+            // le regresamos la interacción a los botones
+            if (sequenceIndex > this.round) {
                 clearInterval(timer);
+
+                setTimeout(() => {
+                    this.blockedButtons = false;
+                }, this.speed);
             }
+
         }, this.speed);
     }
 
-    toggleButtonStyle(button){
-        button.classList.toggle('active');
+    toggleButtonStyle(button) {
+        button.classList.toggle("active");
     }
 
-    gameLost(){
+    gameLost() {
+        alert("¡Fallaste! Intenta de nuevo.");
         this.display.disabled = false;
         this.blockedButtons = true;
     }
 
-    gameWon(){
+    gameWon() {
+        alert("¡Ganaste el juego!");
         this.display.disabled = false;
         this.blockedButtons = true;
     }
 }
 
-
-
-const simon = new Simon(simonButtons, startButton);
+const simon = new Simon();
 simon.init();
